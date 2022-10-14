ca0e1a00f0381a4b093c11a3478a7e4eb8b7699c
Geometer1729 <16kuhnb@gmail.com>
2022-10-14 13:15:39 -0400

    use dot to make a diagram for the whitepaper

--- docs/flake-module.nix
+++ docs/flake-module.nix
@@ -3,11 +3,25 @@
   perSystem = { config, self', inputs', system, ... }:
     let
       pkgs = inputs'.nixpkgs.legacyPackages;
+      diagram = pkgs.stdenv.mkDerivation {
+        buildInputs = with pkgs; [
+          xdot
+        ];
+        name = "diagram";
+        src = ./.;
+        buildPhase = ''
+          dot diagram.dot -Tpng > diagram.png
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp diagram.png $out
+        '';
+      };
     in
     {
       packages = {
         docs = pkgs.stdenv.mkDerivation {
-          name = "build-docs";
+          name = "build-docs" ;
           src = ./.;
 
           buildInputs = with pkgs; [
@@ -22,6 +36,7 @@
           ];
 
           buildPhase = ''
+            cp ${diagram}/diagram.png .
             HOME=$TMP latexmk -output-directory="tmp" -pdf ./*.tex
           '';
           doCheck = false;
