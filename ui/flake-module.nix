dd379d70b058cbe06321d4976acca90c8b646ed6
Hao <hao.liu@platonic.systems>
2022-10-20 08:20:44 +1300

    Switch to dream2nix to build UI

--- ui/flake-module.nix
+++ ui/flake-module.nix
@@ -3,34 +3,19 @@
   perSystem = { config, self', inputs', system, ... }:
     let
       pkgs = inputs'.nixpkgs.legacyPackages;
-      npmlock2nix = pkgs.callPackages self.inputs.npmlock2nix { };
+      inherit (config) dream2nix;
+      ui =
+        (dream2nix.lib.makeOutputs { source = ./.; settings = [{ subsystemInfo.nodejs = 16; }]; }).packages.danaswap-ui;
     in
     {
       packages = {
         danaswap-ui =
-          let
-	    nodeModules = pkgs.symlinkJoin {
-              name = "danaswap-ui-node-modules";
-              paths = [
-                (npmlock2nix.node_modules { src = ./.; })
-              ];
-            };
-          in
           pkgs.runCommand "build-danaswap-ui"
-            { buildInputs = [ pkgs.nodejs-16_x self'.packages."offchain:danaswap-browser" ]; }
+            { buildInputs = [ self'.packages."offchain:danaswap-browser" ]; }
             ''
-              cp ${./build.js} build.js
-              cp ${./routes.json} routes.json
-              cp ${./package.json} package.json
-              cp ${./package-lock.json} package-lock.json
-              cp -r ${./views} views
-              cp -r ${./assets} assets
-              cp -r ${./plugins} plugins
-              cp -r ${nodeModules}/* .
-              node build.js
-              cp ${self'.packages."offchain:danaswap-browser"}/dist/*.js build/assets/scripts/
               mkdir -p $out
-              cp -r build $out/
+              cp -r ${ui}/lib/node_modules/danaswap-ui/build $out && chmod -R +w $out/build
+              cp ${self'.packages."offchain:danaswap-browser"}/dist/*.js $out/build/assets/scripts
             '';
       };
     };
