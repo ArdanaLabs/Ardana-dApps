4550e69225af0e0a0f90dbf152accd79c7621af3
Geometer1729 <16kuhnb@gmail.com>
2022-10-19 19:14:08 -0400

    remove main

--- offchain/danaswap-api/src/Main.purs
+++
@@ -1,7 +0,0 @@
-module Main (main) where
-
-import Contract.Prelude
-
-main :: Effect Unit
-main = log "The api isn't really runable. Maybe you want to run the tests with purs-nix test?"
--- This seems better than throwing a module not found error on purs-nix run
