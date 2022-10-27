f3b2cd644369b40a725e84b0d2346d95ae3dddaf
Hao <hao.liu@platonic.systems>
2022-10-28 10:15:11 +1300

    Address PR feedbacks

---
+++ ui/assets/scripts/main.js
@@ -0,0 +1,29 @@
+// theme toggle
+const themeKey = "danaswap-ui-theme";
+const currentTheme = localStorage.getItem(themeKey);
+const themeCss = document.getElementById("theme-css");
+const themeToggleBtn = document.getElementById("theme-toggle-btn");
+
+if (typeof currentTheme == "undefined" || currentTheme == "night") {
+  themeToggleBtn.firstChild.innerText = "LIGHTMODE";
+  themeCss.href = "./assets/styles/night.css";
+} else {
+  themeToggleBtn.firstChild.innerText = "NIGHTMODE";
+  themeCss.href = "./assets/styles/light.css";
+}
+
+themeToggleBtn.addEventListener("click", function (e) {
+  const currentTheme = localStorage.getItem(themeKey);
+
+  if (typeof currentTheme == "undefined" || currentTheme == "night") {
+    themeToggleBtn.firstChild.innerText = "NIGHTMODE";
+    themeCss.href = "./assets/styles/light.css";
+    localStorage.setItem(themeKey, "light");
+    themeToggleBtn.blur();
+  } else {
+    themeToggleBtn.firstChild.innerText = "LIGHTMODE";
+    themeCss.href = "./assets/styles/night.css";
+    localStorage.setItem(themeKey, "night");
+    themeToggleBtn.blur();
+  }
+});
